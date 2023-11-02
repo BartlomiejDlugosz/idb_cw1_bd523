4f25ade5680ce388cb54007509bd3e3aec4cf54c
Bartlomiej Dlugosz <bd523@ic.ac.uk>
2023-11-02 15:48:03 +0000

    Added first 3 commands

--- idb_2324_cw1.sql
+++ idb_2324_cw1.sql
@@ -1,13 +1,37 @@
 -- Q1 returns (name,born_in,father,mother)
-
+SELECT name,born_in,father,mother
+FROM person
+WHERE born_in IN (SELECT born_in
+                 FROM person AS _
+                 WHERE person.father=name)
+AND born_in IN (SELECT born_in
+               FROM person as _
+               WHERE person.mother=name)
+ORDER BY name
 ;
 
 -- Q2 returns (name)
-
+SELECT name
+FROM person
+EXCEPT
+(SELECT m.name FROM monarch AS m
+UNION
+SELECT p.name FROM prime_minister AS p)
+ORDER BY name
 ;
 
 -- Q3 returns (name)
-
+SELECT name
+FROM monarch
+WHERE 0 < (SELECT COUNT(m.coronation)
+           FROM monarch AS m
+           WHERE m.coronation < (SELECT dod
+                                 FROM person
+                                 WHERE person.name=monarch.name)
+           AND m.coronation > (SELECT coronation
+                               FROM monarch AS m2
+                               WHERE name=monarch.name))
+ORDER BY name
 ;
 
 -- Q4 returns (house,name,accession)
